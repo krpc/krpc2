45cbf9f3a05ce81ebdd43bac025b83cf423849fc
djungelorm <djungelorm@users.noreply.github.com>
2023-03-24 18:01:15 +0000

    dotnet format code

--- service/spacecenter/src/Vessel.cs
+++ service/spacecenter/src/Vessel.cs
@@ -10,33 +10,33 @@ namespace KRPC2.SpaceCenter
     /// orbital and flight data, manipulating control inputs and managing resources.
     /// Created using <see cref="SpaceCenter.ActiveVessel"/> <!--or <see cref="SpaceCenter.Vessels"/>-->.
     /// </summary>
-    [KRPCClass (Service = "SpaceCenter2")]
+    [KRPCClass(Service = "SpaceCenter2")]
     public class Vessel : Equatable<Vessel>
     {
         /// <summary>
         /// Construct from a VesselComponent.
         /// </summary>
-        public Vessel (VesselComponent vessel)
+        public Vessel(VesselComponent vessel)
         {
-            if (ReferenceEquals (vessel, null))
-                throw new ArgumentNullException (nameof (vessel));
+            if (ReferenceEquals(vessel, null))
+                throw new ArgumentNullException(nameof(vessel));
             Id = vessel.GlobalId;
         }
 
         /// <summary>
         /// Returns true if the objects are equal.
         /// </summary>
-        public override bool Equals (Vessel other)
+        public override bool Equals(Vessel other)
         {
-            return !ReferenceEquals (other, null) && Id == other.Id;
+            return !ReferenceEquals(other, null) && Id == other.Id;
         }
 
         /// <summary>
         /// Hash code for the object.
         /// </summary>
-        public override int GetHashCode ()
+        public override int GetHashCode()
         {
-            return Id.GetHashCode ();
+            return Id.GetHashCode();
         }
 
         /// <summary>
